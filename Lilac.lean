8924478f47eaff544494d1b9012e6731b3941d05
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2026-07-13 16:24:43 -0500

    fix Lilac.lean

--- Lilac.lean
+++ Lilac.lean
@@ -1,4 +1,5 @@
--- This module serves as the root of the `Lilac` library.
--- Import modules here that should be built as part of the library.
-import Lilac.Map
-import Lilac.Vec
+module
+
+public import Lilac.Map
+public import Lilac.Vec
+public import Lilac.Tuple
